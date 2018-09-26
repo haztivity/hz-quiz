0e44cc24d1c558516cccfac6b3d9b691ee558999
Javier Pérez <javier.perez@davinchi.es>
2018-09-26 17:17:09 +0200

    chore: build

--- dist/HzQuizResource.js
+++ dist/HzQuizResource.js
@@ -1,7 +1,10 @@
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -249,6 +252,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
         HzQuizResource.prototype.getInstance = function () {
             return this._instance;
         };
+        var HzQuizResource_1;
         HzQuizResource.NAMESPACE = "hzQuiz";
         HzQuizResource.ON_ANSWER = HzQuizResource_1.NAMESPACE + ":answer";
         HzQuizResource.ON_START = HzQuizResource_1.NAMESPACE + ":start";
@@ -274,7 +278,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
             })
         ], HzQuizResource);
         return HzQuizResource;
-        var HzQuizResource_1;
     }(core_1.ResourceController));
     exports.HzQuizResource = HzQuizResource;
 });
